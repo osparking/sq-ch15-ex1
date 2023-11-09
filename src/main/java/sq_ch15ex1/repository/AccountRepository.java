14378b61874417910c62c729cf8f88dcfa309549
osparking <jbpark03@gmail.com>
2023-11-09 19:44:26 +0900

    계좌 저장소 인터페이스

---
+++ src/main/java/sq_ch15ex1/repository/AccountRepository.java
@@ -0,0 +1,20 @@
+package sq_ch15ex1.repository;
+
+import java.math.BigDecimal;
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import sq_ch15ex1.model.Account;
+
+public interface AccountRepository extends CrudRepository<Account, Long>{
+	
+	@Query("select * from account where id=:id")
+	public Account findAccountById(Long id);	
+	
+	@Query("select * from account")
+	public Account findAllAccount();
+	
+	@Query("update account set amount=:amount where id=:id")
+	public void updateAccountAmount(BigDecimal amount, Long id);
+}

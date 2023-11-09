92c0f32a65c0586e529abfd5b1908cbf3eabf515
osparking <jbpark03@gmail.com>
2023-11-09 21:21:11 +0900

    모든 계좌는 목록을 반환

--- src/main/java/sq_ch15ex1/repository/AccountRepository.java
+++ src/main/java/sq_ch15ex1/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package sq_ch15ex1.repository;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -13,7 +14,7 @@ public interface AccountRepository extends CrudRepository<Account, Long>{
 	public Account findAccountById(Long id);	
 	
 	@Query("select * from account")
-	public Account findAllAccount();
+	public List<Account> findAllAccount();
 	
 	@Query("update account set amount=:amount where id=:id")
 	public void updateAccountAmount(BigDecimal amount, Long id);

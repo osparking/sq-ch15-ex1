a3d2f2fd9d91276e43ef387b601e59e06f4c2a32
osparking <jbpark03@gmail.com>
2023-11-09 21:38:12 +0900

    저장소 메소드 수정 애노 적시

--- src/main/java/sq_ch15ex1/repository/AccountRepository.java
+++ src/main/java/sq_ch15ex1/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package sq_ch15ex1.repository;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -17,5 +18,6 @@ public interface AccountRepository extends CrudRepository<Account, Long>{
 	public List<Account> findAllAccount();
 	
 	@Query("update account set amount=:amount where id=:id")
+	@Modifying
 	public void updateAccountAmount(BigDecimal amount, Long id);
 }

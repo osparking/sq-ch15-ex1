af35e7e1b781b706bb6beeea9460c9b7e61ce6c1
osparking <jbpark03@gmail.com>
2023-11-09 15:54:55 +0900

    최초 및 4 종속성

---
+++ src/main/java/sq_ch15ex1/SqCh15Ex1Application.java
@@ -0,0 +1,13 @@
+package sq_ch15ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh15Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh15Ex1Application.class, args);
+	}
+
+}
---
+++ src/test/java/sq_ch15ex1/SqCh15Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch15ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh15Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

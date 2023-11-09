9cf1ff0c81e6ff71d2bf970cdbe2f8d63630bc5f
osparking <jbpark03@gmail.com>
2023-11-09 21:25:00 +0900

    계좌이체 서비스 메소드는 거래임

--- src/main/java/sq_ch15ex1/service/AccountService.java
+++ src/main/java/sq_ch15ex1/service/AccountService.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 import sq_ch15ex1.model.Account;
@@ -18,6 +19,7 @@ public class AccountService {
 		return repository.findAllAccount();
 	}
 	
+	@Transactional
 	public void transferMoney(
 			Long senderId,
 			Long receiverId, 
